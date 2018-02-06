f84140c229babe3482eac86cc11a3863c9a95c93
Lev Sokolov <sokolov.lev95@gmail.com>
2018-02-07 00:10:19 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,18 +1,16 @@
 // swift-tools-version:4.0
-
 import PackageDescription
 
 let package = Package(
-    name: "Pelican",
-    products: [
-			.library(name: "Pelican", targets: ["Pelican"]),
-    ],
-    dependencies: [
-			.package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-			.package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-    ],
-    targets: [
-			.target(name: "Pelican", dependencies: ["Vapor", "FluentProvider"]),
-    ]
+	name: "Pelican",
+	products: [
+		.library(name: "Pelican", targets: ["Pelican"]),
+		],
+	dependencies: [
+		.package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.4.4")),
+		.package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0")),
+		],
+	targets: [
+		.target(name: "Pelican", dependencies: ["Vapor", "FluentProvider"]),
+		]
 )
-
