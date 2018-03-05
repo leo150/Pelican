74c4472aefc9812c079cfeb5b9382a1f2584ae37
Lev Sokolov <sokolov.lev95@gmail.com>
2018-03-05 23:25:32 +0300

    Make MarkupType optional in send method

--- Sources/Pelican/Session/Modules/API Request/Send.swift
+++ Sources/Pelican/Session/Modules/API Request/Send.swift
@@ -28,7 +28,8 @@ public class TGSend {
 	/**
 	Sends a text-based message to the chat linked to this session.
 	*/
-	public func message(_ message: String, markup: MarkupType?, parseMode: MessageParseMode = .markdown, replyID: Int = 0, webPreview: Bool = false, disableNtf: Bool = false) -> Message {
+	@discardableResult 
+	public func message(_ message: String, markup: MarkupType? = nil, parseMode: MessageParseMode = .markdown, replyID: Int = 0, webPreview: Bool = false, disableNtf: Bool = false) -> Message {
 		
 		let request = TelegramRequest.sendMessage(chatID: chatID, text: message, replyMarkup: markup, parseMode: parseMode, disableWebPreview: webPreview, disableNtf: disableNtf, replyMessageID: replyID)
 		let response = tag.sendRequest(request)
@@ -39,7 +40,7 @@ public class TGSend {
 	/**
 	Sends and uploads a file as a message to the chat linked to this session, using a `FileLink`
 	*/
-	public func file(_ file: MessageFile, caption: String, markup: MarkupType?, replyID: Int = 0, disableNtf: Bool = false, callback: ReceiveUpload? = nil) {
+	public func file(_ file: MessageFile, caption: String, markup: MarkupType? = nil, replyID: Int = 0, disableNtf: Bool = false, callback: ReceiveUpload? = nil) {
 		
 		let request = TelegramRequest.sendFile(file: file, callback: nil, chatID: chatID, markup: markup, caption: caption, disableNtf: disableNtf, replyMessageID: replyID)
 		_ = tag.sendRequest(request)
